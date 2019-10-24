be070a3ac5a42aa17328a7452674f4396fa23e62
raoyuandong <837215079@qq.com>
2019-10-24 17:47:09 +0800

    发送大声道

--- app/Helpers/WebSocketHelper.php
+++ app/Helpers/WebSocketHelper.php
@@ -139,6 +139,8 @@ class WebSocketHelper
      */
     public function sendResponseMessage(string $event,$receive,$data){
         if(isset(self::EVENTS[$event])){
+            echo 'fds :'.PHP_EOL;
+            var_dump($receive).PHP_EOL;
             if(!empty($receive)){
                 Websocket::to($receive)->emit(self::EVENTS[$event], $data);
             }else{
