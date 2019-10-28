9a3f05bddb30d05ed4a19385376bed26b1e06caa
gzydong <r.yuandong@qq.com>
2019-10-28 21:29:06 +0800

    asf

--- app/Models/User.php
+++ app/Models/User.php
@@ -29,7 +29,7 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
      * @var array
      */
     protected $fillable = [
-        'mobile', 'nickname','avatarurl','gender','password','created_at'
+        'mobile', 'nickname','avatarurl','gender','password','created_at','motto'
     ];
 
     /**
