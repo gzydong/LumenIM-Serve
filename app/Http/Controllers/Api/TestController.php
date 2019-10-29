9d64764576fc766ce32569576ea01d42b4bcb799
raoyuandong <837215079@qq.com>
2019-10-29 09:10:28 +0800

    FASDA

--- app/Http/Controllers/Api/TestController.php
+++ app/Http/Controllers/Api/TestController.php
@@ -14,11 +14,66 @@ use Illuminate\Support\Facades\DB;
  */
 class TestController
 {
-    public function index(){
-        $arr = [1,2];
 
-        dd(time());
-        dd(date('Y-m-d '));
+    private function getReids(){
+        $redis = new \Redis();
+        $redis->connect('47.105.180.123', 6379); //连接Redis
+        $redis->auth('yd123456'); //密码验证
+        $redis->select(3);//选择数据库2
+
+
+        return $redis;
+    }
+
+
+    public function index(){
+        $arr = [1,2,3];
+
+        $redis = $this->getReids();
+        $goods = [58=>0,59=>0,60=>0,61=>0,62=>0,63=>0,64=>0,65=>0,66=>0,67=>0,68=>0,69=>0];
+        foreach ($goods as $k=>$v){
+            $goods[$k] = $redis->lLen("seckill_{$k}_goods_queue");
+        }
+        dd($goods);
+
+//        $redis->rPush($queue_name, 1);
+//        $redis->rPush($queue_name, 1);
+//        $redis->rPush($queue_name, 1);
+//        exit;
+
+
+//        for ($i=0;$i<3;$i++){
+//            $redis->rPush("seckill_1_goods_queue", 1);
+//        }
+//        for ($i=0;$i<5;$i++){
+//            $redis->rPush("seckill_2_goods_queue", 1);
+//        }
+//        for ($i=0;$i<2;$i++){
+//            $redis->rPush("seckill_3_goods_queue", 1);
+//        }
+//
+//
+//        exit;
+
+        $redis = $this->getReids();
+        $getGoods = [];
+        foreach ($arr as $goods_id){
+            $queue_name = "seckill_{$goods_id}_goods_queue";
+            if($redis->lPop($queue_name) == 1){
+                $getGoods[] = $goods_id;
+            }
+        }
+
+
+        if($getGoods){
+            $str = implode(',',$getGoods);
+            info("秒杀抢购 - 商品ID:{$str}");
+        }
+
+        exit;
+
+
+
         DB::beginTransaction();
         try{
             $total = 0;
