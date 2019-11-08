974c5f3ca6bc780073fd126f6f724ac861f14438
gzydong <r.yuandong@qq.com>
2019-11-08 21:53:20 +0800

    asdfa

--- app/Helpers/Cache/CacheFlag.php
+++ app/Helpers/Cache/CacheFlag.php
@@ -13,7 +13,7 @@ class CacheFlag
     }
 
 
-    public static function chatUnreadNumCacheKey($sender = 0){
-        return $sender == 0 ? 'groups.chat.last.msg' : 'friends.chat.last.msg';
+    public static function chatUnreadNumCacheKey(){
+        return  'hash.chat.unread.num';
     }
 }
