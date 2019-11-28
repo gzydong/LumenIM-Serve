f7d0ee1d5915c987fdb90e571a8449c3cdb3f3e1
raoyuandong <837215079@qq.com>
2019-11-28 14:06:43 +0800

    优化验证方法

--- app/Helpers/common.php
+++ app/Helpers/common.php
@@ -7,13 +7,8 @@
  * @return bool
  */
 function isMobile($mobile) {
-    if (!is_numeric($mobile)) {
-        return false;
-    }
-
-    return preg_match('/^[1][3,4,5,7,8][0-9]{9}$/', $mobile) ? true : false;
+    return (boolean)preg_match('/^[1][3,4,5,7,8][0-9]{9}$/', $mobile);
 }
-
 
 /**
  * 验证登录密码格式
@@ -21,7 +16,7 @@ function isMobile($mobile) {
  * @return bool
  */
 function isPassword($password) {
-    return preg_match('/^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{8,16}$/', $password) ? true : false;
+    return (boolean)preg_match('/^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{8,16}$/', $password);
 }
 
 /**
@@ -31,12 +26,18 @@ function isPassword($password) {
  */
 function checkNumber($num)
 {
-    // 是否为数字标量
-    if (is_numeric($num)) {
-        return preg_match('/^\s*[+-]?\d+\s*$/', $num) || preg_match('/^\s*[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?\s*$/', $num) ;
-    }
+    return is_numeric($num) && (preg_match('/^\s*[+-]?\d+\s*$/', $num) || preg_match('/^\s*[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?\s*$/', $num));
+}
 
-    return false;
+/**
+ * 判断0或正整数
+ * @param string $int 验证字符串
+ * @param bool $isZero  判断是否可为0
+ * @return bool
+ */
+function isInt(string $int,$isZero = false){
+    $reg = $isZero ? '/^[+]{0,1}(\d+)$/' :'/^[1-9]\d*$/';
+    return is_numeric($int) && preg_match($reg, $int);
 }
 
 /**
@@ -228,18 +229,3 @@ function emojiReplace(string $text){
     return $text;
 }
 
-
-/**
- * 判断0或正整数
- * @param string $int 验证字符串
- * @param bool $isZero  判断是否可为0
- * @return bool
- */
-function isInt(string $int,$isZero = false){
-    if (!is_numeric($int)) {
-        return false;
-    }
-
-    $reg = $isZero ? '/^[1-9]\d*|0$/' :'/^[1-9]\d*$/';
-    return preg_match($reg, $int) ? true : false;
-}
