f1530ecb638010669bf48cab7d65493c5cc6f372
yuandong <r.yuandong@qq.com>
2020-03-11 10:25:37 +0800

    优化消息推送

--- app/Http/Controllers/SocketController.php
+++ app/Http/Controllers/SocketController.php
@@ -20,40 +20,48 @@ class SocketController extends Controller
      * @param array $message
      * @return array
      */
-    private function formatMessage(array $message){
+    private function formatMessage(array $message)
+    {
         $data = [
-            'id'=>'',
-            'msg_type'=>'',
-            'source'=>'',
-            'user_id'=>'',
-            'receive_id'=>'',
-            'float'=>'',
-            'avatar'=>'',
-            'nickname'=>'',
-            'friend_remarks'=>'',
-            'send_time'=>'',
-            'content'=>'',
-            'file_id'=>'',
-            'file_original_name'=>'',
-            'file_size'=>'',
-            'file_suffix'=>'',
-            'file_type'=>'',
-            'file_url'=>'',
-            'flie_source'=>'',
-            'save_dir'=>''
+            'send_user' => 0,       //发送消息的用户ID
+            'receive_user' => 0,   //接受者消息ID(用户ID或群ID)
+            'chat_type' => 1,       //聊天类型  1:私聊     2:群聊
+            'msg_type' => 1,        //消息类型  1:文本消息 2:文件消息 3:表情包消息
+            'data' => [
+                'id' => '',
+                'msg_type' => '',
+                'source' => '',
+                'user_id' => '',
+                'receive_id' => '',
+                'float' => '',
+                'avatar' => '',
+                'nickname' => '',
+                'friend_remarks' => '',
+                'send_time' => '',
+                'content' => '',
+                'file_id' => '',
+                'file_original_name' => '',
+                'file_size' => '',
+                'file_suffix' => '',
+                'file_type' => '',
+                'file_url' => '',
+                'flie_source' => '',
+                'save_dir' => ''
+            ]
         ];
 
         return $data;
     }
 
-    private function sendData(){
+    private function sendData()
+    {
         return [
-            'send_user'=>0,       //发送消息的用户ID
-            'receive_user'=> 0,   //接受者消息ID(用户ID或群ID)
-            'chat_type'=>1,       //聊天类型  1:私聊     2:群聊
-            'msg_type'=>1,        //消息类型  1:文本消息 2:文件消息 3:表情包消息
-            'text_message'=>'',   //文本消息内容
-            'file_message'=>0     //文件消息或表情包消息
+            'send_user' => 0,       //发送消息的用户ID
+            'receive_user' => 0,   //接受者消息ID(用户ID或群ID)
+            'chat_type' => 1,       //聊天类型  1:私聊     2:群聊
+            'msg_type' => 1,        //消息类型  1:文本消息 2:文件消息 3:表情包消息
+            'text_message' => '',   //文本消息内容
+            'file_message' => 0     //文件消息或表情包消息
         ];
     }
 
@@ -67,9 +75,6 @@ class SocketController extends Controller
     public function chatDialogue($websocket, $msgData)
     {
         $fd = $msgData['fd'];
-
-        $msgData['send_time'] = date('Y-m-d H:i:s');
-
         //获取客户端绑定的用户ID
         $uid = WebSocketHelper::getFdUserId($fd);
 
@@ -80,7 +85,7 @@ class SocketController extends Controller
         }
 
         //验证消息类型 私聊|群聊
-        if (!in_array($msgData['source_type'], [1, 2]) || !in_array($msgData['msg_type'], [1,2,3,4,5])) {
+        if (!in_array($msgData['source_type'], [1, 2]) || !in_array($msgData['msg_type'], [1, 2, 3, 4, 5])) {
             return true;
         }
 
@@ -99,104 +104,115 @@ class SocketController extends Controller
             }
         }
 
+        //保存的消息记录
+        $record = [
+            'source' => $msgData['source_type'],
+            'msg_type' => 0,
+            'user_id' => $msgData['send_user'],
+            'receive_id' => $msgData['receive_user'],
+            'content' => '',
+            'file_id' => 0,
+            'send_time' => date('Y-m-d H:i:s')
+        ];
 
-        var_dump($msgData);
+        switch ($msgData['msg_type']) {
+            case 1://文本消息
+                $record['msg_type'] = 1;
+                $record["content"] = htmlspecialchars($msgData['text_message']);
+                break;
+            case 2://文件消息
+                $record['msg_type'] = 2;
+                $record['file_id'] = $msgData['file_message'];
+                break;
+            case 3://系统表情包
+                $emoticonDetails = EmoticonDetails::select(['id', 'file_suffix', 'file_size', 'url'])->where('id', $msgData["file_message"])->first();
+                $fileRes = UsersChatFiles::create([
+                    'user_id' => $uid,
+                    'flie_source' => 2,
+                    'file_type' => 1,
+                    'file_suffix' => $emoticonDetails->file_suffix,
+                    'file_size' => $emoticonDetails->file_size,
+                    'save_dir' => $emoticonDetails->url,
+                    'original_name' => '系统表情',
+                    'created_at' => date('Y-m-d H:i:s')
+                ]);
 
-        return true;
-
-        //处理文本消息
-        if ($msgData['msg_type'] == 1) {
-            $msgData["content"] = htmlspecialchars($msgData['content']);
-        } else if ($msgData['msg_type'] == 2) {
-            $fileId = decrypt($msgData["content"]);
-            if (!$fileId)  return true;
-
-            $msgData["content"] = '';
-            $msgData['file_id'] = $fileId;
-        }else if ($msgData['msg_type'] == 5) {
-            $emoticonDetails = EmoticonDetails::select(['id','file_suffix','file_size','url'])->where('id',$msgData["content"])->first();
-            $fileRes = UsersChatFiles::create([
-                'user_id' => $uid,
-                'flie_source'=>2,
-                'file_type' => 1,
-                'file_suffix' => $emoticonDetails->file_suffix,
-                'file_size' => $emoticonDetails->file_size,
-                'save_dir' => $emoticonDetails->url,
-                'original_name' => '系统表情',
-                'created_at' => date('Y-m-d H:i:s')
-            ]);
-
-            $msgData['msg_type'] = 2;
-            $msgData['file_id'] = $fileRes->id;
-            $msgData["content"] = '';
+                $record['msg_type'] = 2;
+                $record['file_id'] = $fileRes->id;
+                break;
         }
 
         //将聊天记录保存到数据库(待优化：后面采用异步保存信息)
-        if (!$insert_id = ChatService::saveChatRecord($msgData)) {
-            info("聊天记录保存失败：" . json_encode($msgData));
+        if (!$insert_id = ChatService::saveChatRecord($record)) {
+            info("聊天记录保存失败：" . json_encode($record));
         }
 
-        //获取消息接收的客户端
-        $clientFds = [];
-        if ($msgData['source_type'] == 1) {//私聊
-            $clientFds = WebSocketHelper::getUserFds($msgData['receive_user']);
-        } else if ($msgData['source_type'] == 2) {
-            $clientFds = WebSocketHelper::getRoomGroupName($msgData['receive_user']);
-        }
+        //推送的数据包
+        $push_message = [
+            'send_user' => $msgData['send_user'],       //发送消息的用户ID
+            'receive_user' => $msgData['receive_user'],   //接受者消息ID(用户ID或群ID)
+            'source_type' => $msgData['source_type'],       //聊天类型  1:私聊     2:群聊
+            'msg_type' => $record['msg_type'],        //消息类型  1:文本消息 2:文件消息
+            'data' => [
+                'id' => $insert_id,//消息记录ID
+                'msg_type' => $record['msg_type'],
+                'source' => $msgData['source_type'],
+                'user_id' => $msgData['send_user'],
+                'receive_id' => $msgData['receive_user'],
+                'send_time' => $record['send_time'],
 
-        //用户信息
-        $userInfo = [
-            'user_id' => $msgData['send_user'],//用户ID
-            'avatar' => '',//用户头像
-            'nickname' => '',//用户昵称
-            'remark_nickname' => ''//好友备注或用户群名片
+                //发送者个人信息
+                'avatar' => '',
+                'nickname' => '',
+                'friend_remarks' => '',
+
+                //文本消息信息
+                'content' => '',
+
+                //文件消息信息
+                'file_id' => $record['file_id'],
+                'file_original_name' => '',
+                'file_size' => '',
+                'file_suffix' => '',
+                'file_type' => '',
+                'file_url' => '',
+                'flie_source' => ''
+            ]
         ];
 
+        $msgData = null;
+
         //获取群聊用户信息
-        if ($msgData['source_type'] == 2) {
-            if ($info = ChatService::getUsersGroupMemberInfo($msgData['receive_user'], $msgData['send_user'])) {
-                $userInfo['avatar'] = $info['avatar'];
-                $userInfo['nickname'] = $info['nickname'];
-                $userInfo['remark_nickname'] = $info['visit_card'];
+        if ($push_message['source_type'] == 2) {
+            if ($info = ChatService::getUsersGroupMemberInfo($push_message['receive_user'], $push_message['send_user'])) {
+                $push_message['data']['avatar'] = $info['avatar'];
+                $push_message['data']['nickname'] = $info['nickname'];
+                $push_message['data']['friend_remarks'] = $info['visit_card'];
             }
         }
-
-        //消息发送者用户信息
-        $msgData['id'] = $insert_id;
-        $msgData['sendUserInfo'] = $userInfo;
-        $msgData["fileInfo"] = [];
 
         //替换表情
-        if ($msgData['msg_type'] == 1) {
-            $msgData["content"] = emojiReplace($msgData['content']);
-        } else if ($msgData['msg_type'] == 2){
-            $fileInfo = UsersChatFiles::where('id', $msgData['file_id'])->first(['file_type', 'file_suffix', 'file_size', 'save_dir', 'original_name']);
-            if ($fileInfo) {
-                $msgData["fileInfo"]['file_type'] = $fileInfo->file_type;
-                $msgData["fileInfo"]['file_suffix'] = $fileInfo->file_suffix;
-                $msgData["fileInfo"]['file_size'] = $fileInfo->file_size;
-                $msgData["fileInfo"]['original_name'] = $fileInfo->original_name;
-                $msgData["fileInfo"]['url'] = $fileInfo->file_type == 1 ? getFileUrl($fileInfo->save_dir) : '';
+        if ($push_message['msg_type'] == 1) {
+            $push_message['data']["content"] = emojiReplace($record['content']);
+        } else if ($push_message['msg_type'] == 2) {
+            if ($fileInfo = UsersChatFiles::where('id', $record['file_id'])->first(['file_type', 'file_suffix', 'file_size', 'save_dir', 'original_name'])) {
+                $push_message['data']['file_type'] = $fileInfo->file_type;
+                $push_message['data']['file_suffix'] = $fileInfo->file_suffix;
+                $push_message['data']['file_size'] = $fileInfo->file_size;
+                $push_message['data']['file_original_name'] = $fileInfo->original_name;
+                $push_message['data']['file_url'] = $fileInfo->file_type == 1 ? getFileUrl($fileInfo->save_dir) : '';
             }
-
-            unset($msgData['file_id']);
-        }else if ($msgData['msg_type'] == 5){
-            $fileInfo = EmoticonDetails::where('id', $msgData['file_id'])->first(['url']);
-            if ($fileInfo) {
-                $msgData["fileInfo"]['file_type'] = 1;
-                $msgData["fileInfo"]['file_suffix'] = '';
-                $msgData["fileInfo"]['file_size'] = '';
-                $msgData["fileInfo"]['original_name'] = '';
-                $msgData["fileInfo"]['url'] = getFileUrl($fileInfo->url);
-            }
-
-            unset($msgData['file_id']);
         }
 
-        unset($msgData['fd']);
+        //获取消息推送的客户端
+        $clientFds = [];
+        if ($push_message['source_type'] == 1) {//私聊
+            $clientFds = WebSocketHelper::getUserFds($push_message['receive_user']);
+        } else if ($push_message['source_type'] == 2) {
+            $clientFds = WebSocketHelper::getRoomGroupName($push_message['receive_user']);
+        }
 
-        //发送消息
-        WebSocketHelper::sendResponseMessage('chat_message', $clientFds, $msgData);
+        WebSocketHelper::sendResponseMessage('chat_message', $clientFds, $push_message);
     }
 
     /**
