69323807c0034011581f164896dad9dcc3417448
YuanDong <r.yuandong@qq.com>
2020-09-09 17:35:03 +0800

    优化代码

--- config/config.php
+++ config/config.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: admin
- * Date: 2019/9/20
- * Time: 15:11
- */
 return [
     //设置文件图片访问的域名
     'file_domain'=>'http://im-img.gzydong.club',
@@ -14,6 +8,11 @@ return [
     'swoole_proxy'=>[
         'host'=>'127.0.0.1',
         'port'=>'9501'
+    ],
+
+    'jwt'=>[
+        'secret'=>env('JWT_SECRET',''),
+        'ttl'=>60*60*24*7
     ],
 
     'web_url'=>'http://im.gzydong.club',
